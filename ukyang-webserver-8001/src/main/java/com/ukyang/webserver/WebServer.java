8841e84c4e59df646ab9e42a14fa8d92c9de950a
ukyang <ukyang_ma@163.com>
2022-09-27 00:37:13 +0800

    [update] update child module —— webserver8001
    修改 webserver8001 子工程为 eureka 客户端

--- ukyang-webserver-8001/src/main/java/com/ukyang/webserver/WebServer.java
+++ ukyang-webserver-8001/src/main/java/com/ukyang/webserver/WebServer.java
@@ -2,6 +2,7 @@ package com.ukyang.webserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * WebServer8001 主启动类
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @since 2022-09-26 23:58
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class WebServer {
     public static void main(String[] args) {
         SpringApplication.run(WebServer.class, args);
